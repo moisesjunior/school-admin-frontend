4f64bf488e224d8f8da73eed83a263acdc0811f9
Moisés Junior <nem.moises@hotmail.com>
2021-04-24 18:48:49 -0300

    [FEAT] Instalando as dependencias do Amplify e configurando a pool do Cognito

---
+++ src/services/AmplifySetup.ts
@@ -0,0 +1,12 @@
+import Amplify from 'aws-amplify';
+
+export default Amplify.configure({
+  Auth: {
+    region: process.env.REACT_APP_AWS_REGION,
+    identityPoolRegion: process.env.REACT_APP_AWS_REGION,
+    userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL,
+    userPoolWebClientId: process.env.REACT_APP_AWS_COGNITO_WEB_CLIENT_ID,
+    mandatorySignIn: false,
+    authenticationFlowType: "USER_PASSWORD_AUTH"
+  }
+});
