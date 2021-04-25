2e3f4364b0e3d59fbdd10a01593ec889fb1c8314
Moisés Junior <nem.moises@hotmail.com>
2021-04-24 22:12:33 -0300

    [FIX] Inserindo atributo de nome no UIStore para persistencia

--- src/services/Store.ts
+++ src/services/Store.ts
@@ -2,8 +2,10 @@ import { Store } from 'pullstate';
 
 interface UIStoreState {
   signed: boolean;
+  name: string;
 }
 
 export const UIStore = new Store<UIStoreState>({
-  signed: Boolean(localStorage.getItem('signed')) || false
+  signed: Boolean(localStorage.getItem('signed')) || false,
+  name: localStorage.getItem('name') || ''
 });
