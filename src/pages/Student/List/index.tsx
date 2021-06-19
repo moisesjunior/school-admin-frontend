f0c3318b658cac809ab293cbc95d4355bb8dbb3e
Moises Junior <moises@smartinnovation.com.br>
2021-06-19 03:45:17 -0300

    Finalizando filtro de estudantes

--- src/pages/Student/List/index.tsx
+++ src/pages/Student/List/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import ContentPage from '../../../components/ContentPage';
 import EnhancedTable from '../../../components/Table';
 import VisibilityIcon from '@material-ui/icons/Visibility';
@@ -7,8 +7,64 @@ import CreateIcon from '@material-ui/icons/Create';
 import { Auth } from 'aws-amplify';
 import api from '../../../services/api';
 import Swal from 'sweetalert2';
+import Filter from '../../../components/Filter';
+import { useState } from 'react';
+import { Course } from './index.d';
+import { useEffect } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@material-ui/core';
+import { maskCPF } from '../../../utils/mask';
 
 const ListStudent = (): JSX.Element => {
+  const [ url, setUrl ] = useState('/customer');
+  const [ nameSearch, setNameSearch ] = useState('');
+  const [ cpfSearch, setCpfSearch ] = useState('');
+  const [ courseSearch, setCourseSearch ] = useState('');
+  const [ statusSearch, setStatusSearch ] = useState('');
+  const [ page, setPage ] = useState(0);
+  const [ courses, setCourses ] = useState<Course[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const userTokens = await Auth.currentSession();
+      const response = await api.get('/course', {
+        headers: {
+          'CognitoIdToken': userTokens.getIdToken().getJwtToken()
+        }
+      });
+      setCourses(response.data);
+    }
+
+    getData();
+  }, []);
+
+  const handleFilter = (e: FormEvent) => {
+    e.preventDefault();
+    var params = new URLSearchParams();
+
+    if (nameSearch !== ''){
+      params.set("name", nameSearch);
+    }
+    if (cpfSearch !== ''){
+      params.set("cpf", cpfSearch);
+    }
+    if (courseSearch !== ''){
+      params.set("course", courseSearch);
+    }
+    if (statusSearch !== ''){
+      params.set("status", statusSearch);
+    }
+    setUrl('/customer?' + params.toString());
+  }
+
+  const handleClear = () => {
+    setUrl('/customer');
+    setNameSearch('');
+    setCpfSearch('');
+    setCourseSearch('');
+    setStatusSearch('');
+    setPage(0);
+  }
+
   const handleDelete = async (id?: string) => {
     const currentSession = await Auth.currentSession();
 
@@ -50,9 +106,55 @@ const ListStudent = (): JSX.Element => {
 
   return (
     <ContentPage>
+      <Filter onCancel={handleClear} onSubmit={handleFilter} name="Filtro de alunos">
+        <FormControl variant="outlined">
+          <InputLabel id="labelType">Curso</InputLabel>
+          <Select
+            labelId="labelType"
+            label="Curso"
+            value={courseSearch}
+            onChange={(e) => setCourseSearch(e.target.value as string)}
+          >
+            <MenuItem value=""><em>None</em></MenuItem>
+            {courses.map(course => (
+              <MenuItem value={course.id}>
+                {course.description}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField 
+          variant="outlined"
+          value={nameSearch}
+          onChange={(e) => setNameSearch(e.target.value)}
+          placeholder="Nome do aluno"
+        />
+        <TextField 
+          variant="outlined"
+          value={maskCPF(cpfSearch)}
+          onChange={(e) => setCpfSearch(maskCPF(e.target.value))}
+          placeholder="CPF"
+        />
+        <FormControl variant="outlined">
+          <InputLabel id="labelType">Status</InputLabel>
+          <Select
+            labelId="labelType"
+            label="Status"
+            value={statusSearch}
+            onChange={(e) => setStatusSearch(e.target.value as string)}
+          >
+            <MenuItem value=""><em>None</em></MenuItem>
+            <MenuItem value="ativo">Ativo</MenuItem>
+            <MenuItem value="trancado">Matrícula trancada</MenuItem>
+            <MenuItem value="reprovado">Reprovado</MenuItem>
+            <MenuItem value="desistente">Desistente</MenuItem>
+          </Select>
+        </FormControl>
+      </Filter>
       <EnhancedTable
+        page={page}
         name="Alunos"
-        url="/customer"
+        url={url}
         title="ADICIONAR ALUNO"
         formUrl="/student"
         headCells={[
